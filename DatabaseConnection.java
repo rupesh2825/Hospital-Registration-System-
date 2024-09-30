593a58578d4e562d6c7cbd7aaf9d243b17b9c2b5
Rupesh Ravsaheb Chavan <152914283+rupesh2825@users.noreply.github.com>
2024-10-01 01:24:37 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "ra.one";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
